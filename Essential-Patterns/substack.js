be871964c17f834cccf5cad628661c80d7a35d0b
Sal P <salvatpalvat@gmail.com>
2016-11-30 00:21:19 -0500

    Design Pattern

---
+++ Essential-Patterns/substack.js
@@ -0,0 +1,12 @@
+/**
+        The substack pattern grants a single entrance point into a module. Simplicity is the guiding principle behind this practice, which is a major aim of programming in general.
+**/
+
+module.exports = (name) => {
+    if (typeof name === "string") {
+        console.log(`Your name is ${name}`)
+    }
+    else {
+        console.log("Please enter a string")
+    }
+}
