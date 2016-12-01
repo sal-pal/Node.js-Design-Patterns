6ab858dffa84023670b211ca5c1360242f134e71
Sal P <salvatpalvat@gmail.com>
2016-12-01 00:00:23 -0500

    Design Pattern

--- Essential-Patterns/small-surface-area.js
+++ Essential-Patterns/small-surface-area.js
@@ -1,8 +1,10 @@
 /**
-        An example of the small surface area pattern, where                   responseManipulator is a response haandler granting clients
+        An example of the small surface area pattern, where                  
+        responseManipulator is a response haandler granting clients
         access to manipulating the response through a callback. 
         Clients also have access to other functionality via the Event
-        Emitter object, such as manipulating the output of the data           event handler during the end event.
+        Emitter object, such as manipulating the output of the data 
+	event handler during the end event.
 **/
 
 var responseManipulator = (req, res, callback) => {
