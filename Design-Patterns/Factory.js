288f5181bd6ce7734f3eefd9533a073c45cb42dd
Sal P <salvatpalvat@gmail.com>
2017-02-09 23:06:48 -0500

    Implementation of Factory Pattern

---
+++ Design-Patterns/Factory.js
@@ -0,0 +1,57 @@
+module.exports = (length) => {
+    var array = []
+    for (var i=0; i < length+1; i++) {
+        array.push(chooseRandomPrimitive())
+    }
+    return array
+}
+
+
+
+
+function chooseRandomPrimitive () {
+    const choiceValue = makeChoiceValue()
+    if (choiceValue <= 30) {
+        return makeRandomNumber()
+    }
+    else if (choiceValue > 30 && choiceValue <= 60) {
+        return makeRandomString()
+    }
+    else {return makeRandomBool()}
+}
+
+
+
+
+function makeChoiceValue () {     
+    var choiceValue = 91
+    while (choiceValue > 90) {
+        choiceValue = makeRandomNumber()
+    }
+    return choiceValue
+}
+
+
+
+
+function makeRandomNumber () {
+    return Math.round(Math.random() * 100)
+}
+
+
+
+
+function makeRandomString () {
+    const randomString = require("randomstring")
+    return randomString.generate(5)
+}
+
+
+
+
+function makeRandomBool () {
+    if (makeRandomNumber() < 50) {
+        return true
+    }
+    else {return false}
+}
